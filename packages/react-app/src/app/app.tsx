209f21cf54dee3665180fc558d51bd5e0c07d175
Bernardo Cardoso <bernardo.cardoso@outsystems.com>
2023-07-25 09:32:31 +0100

    added script to update osui package

--- packages/react-app/src/app/app.tsx
+++ packages/react-app/src/app/app.tsx
@@ -1,6 +1,6 @@
-import { OsuiCard } from 'core-components-react';
+import { OsuiCard, OsuiButton } from 'core-components-react';
 // eslint-disable-next-line @nx/enforce-module-boundaries
-import { alignment, border } from '../../../../node_modules/outsystems-stencil/src/components/globalEnum';
+import { alignment, border } from '../../../globalEnum';
 import './../styles.scss';
 import { setupIonicReact } from '@ionic/react';
 
@@ -12,8 +12,8 @@ export function App() {
     <div style={{ width: '300px' }}>
       <OsuiCard
         alignment={alignment.vertical} reversecolumn={false} border={border.rounded} backgroundcolor={"#fff"} showheader={true}>
-
         <img height="+250px" width="100%" slot="content" src="https://outsystemsui.outsystems.com/OutSystemsUIWebsite/img/StyleGuidePreviewAssets.PatternPreviewPhoto10.png" alt="" />
+        <OsuiButton slot="footer">I'm a Custom Ionic Button</OsuiButton>
       </OsuiCard>
     </div>
   );
