0def6d85db95c98ab2119c7343335b31cabdb177
Bernardo Cardoso <bernardo.cardoso@outsystems.com>
2023-07-27 16:06:19 +0100

    Revert "Updated refrences to ion icons"
    This reverts commit c17b0246f53bfd272bdb3c2d7de2e361d2e03120.

--- packages/react-app/src/app/app.tsx
+++ packages/react-app/src/app/app.tsx
@@ -11,10 +11,9 @@ export function App() {
   return (
     <div style={{ width: '300px' }}>
       <OsuiCard
-        alignment={alignment.vertical} reversecolumn={false} border={border.rounded} backgroundcolor={"#fff"} showheader={true}>
-        <p slot="header">Custom Stencil Component</p>
+        alignment={alignment.vertical} reverse-column={false} border={border.rounded} background-color={"#fff"} show-header={true}>
         <img height="+250px" width="100%" slot="content" src="https://outsystemsui.outsystems.com/OutSystemsUIWebsite/img/StyleGuidePreviewAssets.PatternPreviewPhoto10.png" alt="" />
-        <OsuiButton slot="footer"></OsuiButton>
+        <OsuiButton slot="footer">I'm a Custom Ionic Button</OsuiButton>
       </OsuiCard>
     </div>
   );
