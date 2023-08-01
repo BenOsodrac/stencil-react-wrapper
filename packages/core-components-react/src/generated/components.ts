016f1e4a894e27a3e169ab828c3c1e1f62cfebac
Bernardo Cardoso <bernardo.cardoso@outsystems.com>
2023-08-01 15:32:42 +0100

    updated package

---
+++ packages/core-components-react/src/generated/components.ts
@@ -0,0 +1,14 @@
+/* eslint-disable */
+/* tslint:disable */
+/* auto-generated react proxies */
+import { createReactComponent } from './react-component-lib';
+
+import type { JSX } from 'outsystems-stencil';
+
+import { defineCustomElements } from 'outsystems-stencil/loader';
+
+defineCustomElements();
+export const OsuiButton = /*@__PURE__*/createReactComponent<JSX.OsuiButton, HTMLOsuiButtonElement>('osui-button');
+export const OsuiCard = /*@__PURE__*/createReactComponent<JSX.OsuiCard, HTMLOsuiCardElement>('osui-card');
+export const OsuiRangeSlider = /*@__PURE__*/createReactComponent<JSX.OsuiRangeSlider, HTMLOsuiRangeSliderElement>('osui-range-slider');
+export const OsuiRating = /*@__PURE__*/createReactComponent<JSX.OsuiRating, HTMLOsuiRatingElement>('osui-rating');
